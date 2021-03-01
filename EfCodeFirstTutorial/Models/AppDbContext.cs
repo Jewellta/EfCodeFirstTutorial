c1a459fbcdc555e03d9a14162837f690aa211326
Tyler Jewell <jewelltyler15@gmail.com>
2021-03-01 13:24:23 -0500

    Add project files.

---
+++ EfCodeFirstTutorial/Models/AppDbContext.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.EntityFrameworkCore;
+
+namespace EfCodeFirstTutorial.Models
+{
+	public class AppDbContext : DbContext
+	{
+		public DbSet<Customer> Customers { get; set; }
+		public AppDbContext(){
+		}
+		protected override void OnConfiguring(DbContextOptionsBuilder builder) {
+			if (!builder.IsConfigured) {
+				var connStr = "server=localhost\\sqlexpress;" +
+					"database=AppDb1;" +
+					"trusted_connection=true;";
+				builder.UseSqlServer(connStr);
+					}
+		}
+
+		protected override void OnModelCreating(ModelBuilder builder){
+
+		}
+	}
+}
---
+++ EfCodeFirstTutorial/Models/Customer.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text;
+
+namespace EfCodeFirstTutorial.Models
+{
+	public class Customer
+	{
+		public int Id { get; set; }	//primary key
+		[StringLength(10), Required]
+		public string Code { get; set; } // must be unique value
+		[StringLength(50), Required]
+		public string Name { get; set; }
+		public bool IsNational { get; set; }	
+		[Column(TypeName="decimal(9,2)")]
+		public decimal Sales { get; set; }        //default set to zero
+		public DateTime Created { get; set; }
+
+		public Customer(){
+		}
+	}
+}
